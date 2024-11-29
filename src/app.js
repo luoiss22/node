00be8b35f2d0f13c3d7e41f45ecf6708bf62235e
NeriG03 <sosavillanuevan@gmail.com>
2024-11-29 01:50:56 -0600

    Update database initialization and configuration
    - Change DB_PASSWORD in .env for security
    - Update main entry point in package.json
    - Refactor app.js to initialize the database before starting the server
    - Enhance database connection handling in index.models.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import routes from "../routes/index.routes.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import routes from '../routes/index.routes.js';
+import { initializeDatabase } from './models/index.models.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 routes(app);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+initializeDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}).catch(error => {
+    console.error('Failed to initialize database:', error);
 });
--- src/models/index.models.js
+++ src/models/index.models.js
@@ -2,14 +2,9 @@ import { Sequelize } from "sequelize";
 import config from "../config/config.js";
 import { Paciente, PacienteSchema } from "./paciente.model.js";
 
-
-
-function setUpModels(sequelize){
+function setUpModels(sequelize) {
     Paciente.init(PacienteSchema, Paciente.config(sequelize));
-
-
-    //RELACIONES
-
+    // RELACIONES
 }
 
 const sequelize = new Sequelize(
@@ -21,11 +16,20 @@ const sequelize = new Sequelize(
     dialect: 'postgres'
 });
 
-sequelize.sync();
-setUpModels(sequelize);
-
-export {
-    setUpModels
+async function initializeDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        setUpModels(sequelize);
+        await sequelize.sync({ force: true }); // Use force: true to drop and recreate tables
+        console.log('All models were synchronized successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        throw error; // Ensure the error is propagated
+    }
 }
 
-
+export {
+    setUpModels,
+    initializeDatabase
+}
